65590c90ba0d02d222fd20067e6a0fb6b7e178d1
Rii-Dev <frasyiqpramana@gmail.com>
2026-05-31 16:13:37 +0700

    Adding Void Mail

--- data/projects.js
+++ data/projects.js
@@ -14,10 +14,18 @@ export const projects = [
     category: "tool",
     type: "OSINT Tool",
     title: "Just Dork It - Google Dork Generator",
-    desc:
-      "Tool ringan untuk menyusun Google Dork queries secara cepat, dirancang untuk workflow OSINT dan reconnaissance yang rapi dan efisien.",
+    desc: "Tool ringan untuk menyusun Google Dork queries secara cepat, dirancang untuk workflow OSINT dan reconnaissance yang rapi dan efisien.",
     tags: ["OSINT", "Google Dork", "Web Tool", "Reconnaissance"],
     link: "https://ikanparii.github.io/Just-Dork-It/",
+    cta: "Try Tool",
+  },
+  {
+    category: "tool",
+    type: "Temporary Mail Tool",
+    title: "Void Mail Temporary Email Generator",
+    desc: "VoidMail adalah aplikasi temporary mail berbasis privasi yang digunakan untuk membuat alamat email sementara tanpa registrasi",
+    tags: ["Web Development, Privacy"],
+    link: "https://void-mail.ikanparii.workers.dev/",
     cta: "Try Tool",
   },
   // Writeup atau blog yang berisi dokumentasi belajar.
@@ -25,8 +33,7 @@ export const projects = [
     category: "writeup",
     type: "CTF Writeups",
     title: "Just-Blog - CTF Writeup Vault",
-    desc:
-      "Vault writeup utama berisi analisis CTF, catatan forensik, dan dokumentasi pembelajaran cybersecurity yang dikurasi dengan gaya editorial.",
+    desc: "Vault writeup utama berisi analisis CTF, catatan forensik, dan dokumentasi pembelajaran cybersecurity yang dikurasi dengan gaya editorial.",
     tags: ["Blog", "Writeup", "CTF", "Blue Team"],
     link: "https://ikanparii.github.io/just-blog/",
     cta: "Visit Blog",
